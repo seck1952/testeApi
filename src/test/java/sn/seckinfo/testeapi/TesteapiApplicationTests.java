ee2f7de621505838fab492392c7010b829b71d15
elhadji <seckmoustapha91@gmail.com>
2024-05-14 13:36:04 +0000

    first commit

---
+++ src/test/java/sn/seckinfo/testeapi/TesteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.seckinfo.testeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
